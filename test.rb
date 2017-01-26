5b8ddf836e6dd2b28eaa951ec608d46c13dd208c
Jamie Locatis <jlocatis@gmail.com>
2017-01-25 18:39:10 -0600

    Late Wednesday Changes

--- test.rb
+++ test.rb
@@ -43,75 +43,73 @@ def printStuff(string, variable)
 	puts string + (" "*line) + "|" + variable[0] + (" "*line2) + "|" + variable[1]
 end
 
-pAllowance = totalSpent('Priya', 'Allowance')
-sAllowance = totalSpent('Sonia', 'Allowance')
-pCarRepairs = totalSpent('Priya', 'Car Repairs')
-#sCarRepairs = totalSpent('Sonia', 'Car Repairs')
-pRent = totalSpent('Priya', 'Rent')
-#sRent = totalSpent('Sonia', 'Rent')
-pEntertainment = totalSpent('Priya', 'Entertainment')
-sEntertainment = totalSpent('Sonia', 'Entertainment')
-pFuel = totalSpent('Priya', 'Fuel')
-#sFuel = totalSpent('Sonia', 'Fuel')
-pClothes = totalSpent('Priya', 'Clothes')
-sClothes = totalSpent('Sonia', 'Clothes')
-pEducation = totalSpent('Priya', 'Education')
-sEducation = totalSpent('Sonia', 'Education')
-pGroceries = totalSpent('Priya', 'Groceries')
-sGroceries = totalSpent('Sonia', 'Groceries')
-pGifts = totalSpent('Priya', 'Gifts')
-sGifts = totalSpent('Sonia', 'Gifts')
-pUtilities = totalSpent('Priya', 'Utilities')
-sUtilities = totalSpent('Sonia', 'Utilities')
-#pGym = totalSpent('Priya', 'Gym')
-sGym = totalSpent('Sonia', 'Gym')
-#pMedicalDental = totalSpent('Priya', 'Medical/Dental')
-sMedicalDental = totalSpent('Sonia', 'Medical/Dental')
-#pHouseholdGoods = totalSpent('Priya', 'Household Goods')
-sHouseholdGoods = totalSpent('Sonia', 'Household Goods')
 pBalance = totalSpent('Priya', 'Balance')
 sBalance = totalSpent('Sonia', 'Balance')
 
 ## DISPLAY CODE
 
-#inputName = ARGV
+inputName = ARGV
 
-#if inputName[0] === 'Priya'
-puts ("*" * 80)
-puts "Account: Priya *** Balance: " + pBalance[0]
-puts ("*" * 80)
-puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
-puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
-printStuff("Allowance", pAllowance)
-printStuff("Car Repairs", pCarRepairs)
-printStuff("Rent", pRent)
-printStuff("Entertainment", pEntertainment)
-#printStuff("Household Goods", pHouseholdGoods)
-printStuff("Fuel", pFuel)
-printStuff("Clothes", pClothes)
-printStuff("Education", pEducation)
-printStuff("Groceries", pGroceries)
-printStuff("Gifts", pGifts)
-#printStuff("Gym", pGym)
-# printStuff("Medical/Dental", pMedicalDental)
-printStuff("Utilities", pUtilities)
+if inputName[0] === 'Priya'
+	puts ("*" * 80)
+	puts "Account: Priya *** Balance: " + pBalance[0]
+	puts ("*" * 80)
+	puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
+	puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
 
-#if inputName[1] === 'Sonia'
-puts ("*" * 80)
-puts "Account: Sonia *** Balance: " + sBalance[0]
-puts ("*" * 80)
-puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
-puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
-printStuff("Allowance", sAllowance)
-# printStuff("Car Repairs", sCarRepairs)
-# printStuff("Rent", sRent)
-printStuff("Entertainment", sEntertainment)
-printStuff("Household Goods", sHouseholdGoods)
-# printStuff("Fuel", sFuel)
-printStuff("Clothes", sClothes)
-printStuff("Education", sEducation)
-printStuff("Groceries", sGroceries)
-printStuff("Gifts", sGifts)
-printStuff("Gym", sGym)
-printStuff("Medical/Dental", sMedicalDental)
-printStuff("Utilities", sUtilities)
+	categories = ["Allowance", "Car Repairs", "Rent", "Entertainment", "Fuel", "Clothes",
+	"Education", "Groceries", "Gifts", "Utilities"]
+	x = 0
+	while x <= categories.count - 1
+		tempArray = totalSpent('Priya', categories[x])
+		printStuff(categories[x], tempArray)
+		x += 1
+	end
+
+elsif inputName[0] === 'Sonia'
+	puts ("*" * 80)
+	puts "Account: Sonia *** Balance: " + sBalance[0]
+	puts ("*" * 80)
+	puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
+	puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
+
+	categories = ["Allowance", "Entertainment", "Clothes", "Medical/Dental", "Gym",
+	"Education", "Groceries", "Gifts", "Utilities", "Household Goods"]
+	x = 0
+	while x <= categories.count - 1
+		tempArray = totalSpent('Sonia', categories[x])
+		printStuff(categories[x], tempArray)
+		x += 1
+	end
+
+else
+	puts ("*" * 80)
+	puts "Account: Priya *** Balance: " + pBalance[0]
+	puts ("*" * 80)
+	puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
+	puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
+
+	categories = ["Allowance", "Car Repairs", "Rent", "Entertainment", "Fuel", "Clothes",
+	"Education", "Groceries", "Gifts", "Utilities"]
+	x = 0
+	while x <= categories.count - 1
+		tempArray = totalSpent('Priya', categories[x])
+		printStuff(categories[x], tempArray)
+		x += 1
+	end
+
+	puts ("*" * 80)
+	puts "Account: Sonia *** Balance: " + sBalance[0]
+	puts ("*" * 80)
+	puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
+	puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
+
+	categories = ["Allowance", "Entertainment", "Clothes", "Medical/Dental", "Gym",
+	"Education", "Groceries", "Gifts", "Utilities", "Household Goods"]
+	x = 0
+	while x <= categories.count - 1
+		tempArray = totalSpent('Sonia', categories[x])
+		printStuff(categories[x], tempArray)
+		x += 1
+	end		
+end					
--- test2.rb
+++ test2.rb
@@ -12,8 +12,7 @@ def totalSpent(personName, category)
 				inflow = row['Inflow'].delete('$').delete(',').to_f
 				outflow = row['Outflow'].delete('$').delete(',').to_f
 				tempArray << inflow - outflow
-			end
-			if category === "Balance"
+			elsif category === "Balance"
 				inflow = row['Inflow'].delete('$').delete(',').to_f
 				outflow = row['Outflow'].delete('$').delete(',').to_f
 				tempArray << inflow - outflow
@@ -22,72 +21,61 @@ def totalSpent(personName, category)
 	end
 
 	def avgTrans(array)
-		binding.pry
 		count = array.count
-		binding.pry
 		array = array.inject(:+)
-		binding.pry
 		array = array / count
-		binding.pry
 		return array
 	end
 
+	tempArray.compact
+	if tempArray == []
+		tempArray << 0
+	end
+
 	average = avgTrans(tempArray)
-	binding.pry
 	tempArray = tempArray.inject(:+)
-	binding.pry
 	average = "$" + average.round(2).to_s
-	binding.pry
 	tempArray = "$" + tempArray.round(2).to_s
-	binding.pry
-	returnArray << tempArray
-	binding.pry
-	returnArray << average
-	binding.pry
+	returnArray << tempArray << average
+#	returnArray << average
 	return returnArray
 end
 
 def printStuff(string, variable)
-	line = 28 - string.length
-	line2 = 13 - variable[0].length
-	puts string + (" "*line) + "|" + variable[0] + (" "*line2) + "|" + variable[1]
+	if variable[0] != "$0.0"
+		line = 28 - string.length
+		line2 = 13 - variable[0].length
+		puts string + (" "*line) + "|" + variable[0] + (" "*line2) + "|" + variable[1]
+	end
 end
 
-pBalance = totalSpent('Priya', 'Balance')
-sBalance = totalSpent('Sonia', 'Balance')
+def printMoreStuff(inputName)
+	balance = totalSpent(inputName, 'Balance')
+	puts ("*" * 80)
+	puts "Account: " + inputName + " *** Balance: " + balance[0]
+	puts ("*" * 80)
+	puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
+	puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
+
+	categories = ["Allowance", "Car Repairs", "Rent", "Entertainment", "Fuel", "Clothes",
+	"Education", "Groceries", "Gifts", "Utilities", "Household Goods", "Gym", "Medical/Dental", "Meals"]
+	x = 0
+	while x <= categories.count - 1
+		finalArray = totalSpent(inputName, categories[x])
+		printStuff(categories[x], finalArray)
+		x += 1
+	end
+end
 
 ## DISPLAY CODE
 
-# inputName = ARGV
+inputName = ARGV
 
-# if inputName[0] === 'Priya'
-puts ("*" * 80)
-puts "Account: Priya *** Balance: " + pBalance[0]
-puts ("*" * 80)
-puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
-puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
-
-categories = ["Allowance", "Car Repairs", "Rent", "Entertainment", "Fuel", "Clothes",
-"Education", "Groceries", "Gifts", "Utilities"]
-x = 0
-while x <= categories.count - 1
-	tempArray = totalSpent('Priya', categories[x])
-	printStuff(categories[x], tempArray)
-	x += 1
+if inputName[0] === 'Priya'
+	printMoreStuff('Priya')
+elsif inputName[0] === 'Sonia'
+	printMoreStuff('Sonia')
+else
+	printMoreStuff('Priya')
+	printMoreStuff('Sonia')
 end
-
-# if inputName[1] === 'Sonia'
-puts "Account: Sonia *** Balance: " + sBalance[0]
-puts ("*" * 80)
-puts "Category" + (" "*20) + "| Total Spent | Average Transaction" + (" " * 17)
-puts ("-" * 28) + "|" + ("-" * 13) + "|" + ("-" * 37)
-
-categories = ["Allowance", "Entertainment", "Clothes", "Medical/Dental", "Gym",
-"Education", "Groceries", "Gifts", "Utilities", "Household Goods"]
-x = 0
-while x <= categories.count - 1
-	tempArray = totalSpent('Sonia', categories[x])
-	printStuff(categories[x], tempArray)
-	x += 1
-end
-									
